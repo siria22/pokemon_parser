600d501120195e97012c12d8d7faf5d6ec364160
Sirius_Alpha <scgv2222@naver.com>
2024-08-12 00:47:56 +0900

    ver 0.0.2 Excel export function added

--- csvExtractor.js
+++ csvExtractor.js
@@ -1,9 +1,5 @@
-function arrayToCSV(data) {
-    return data.map(row => row.join(",")).join("\n");
-}
-
 function downloadCSV(csvData, filename = 'data.csv') {
-    const blob = new Blob([csvData], { type: 'text/csv' });
+    const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
@@ -16,7 +12,13 @@ function downloadCSV(csvData, filename = 'data.csv') {
 
 document.getElementById('btn_extractAsCsv')
         .addEventListener('click', function() {
-                const csvData = arrayToCSV(EXTRACTED_POKEMON);
+                const csvData = EXTRACTED_POKEMON;
                 downloadCSV(csvData);
             }
         );
+
+
+function pkmDTO(pkmInstance){
+    let data = `${pkmInstance.name},${pkmInstance.pokeType1_kor},${pkmInstance.pokeType2_kor},${pkmInstance.fmove.name_kor},${pkmInstance.cmove.name_kor},${pkmInstance.dps},${pkmInstance.tdo},${pkmInstance.overall},${pkmInstance.cp},\n`;
+    return data;
+}
